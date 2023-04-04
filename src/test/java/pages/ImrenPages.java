3461ba19e9ae9d1439d9d2a670d7374c0494d893
ImrenAsan <imrenasan@protonmail.com>
2023-04-04 15:34:13 +0200

    US002 completed!

---
+++ src/test/java/pages/ImrenPages.java
@@ -0,0 +1,53 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class ImrenPages {
+
+    public ImrenPages() {
+        PageFactory.initElements(Driver.getDriver(),  this);}
+
+    //RegisterPage - US_002
+    @FindBy(xpath = "/html/body/div[1]/div/div[2]/div[2]/nav/div/ul/li[8]")
+    public WebElement dropdownClick;
+
+    @FindBy (xpath = "/html/body/div/div/div[2]/div[2]/nav/div/ul/li[8]/div/a[2]")
+    public WebElement register;
+
+
+    @FindBy(xpath = "(//input[@id='ssn'])[1]")
+    public WebElement ssn;
+
+    @FindBy(xpath = "//input[@id='firstname']")
+    public WebElement firstName;
+
+    @FindBy(xpath = "//input[@id='lastname']")
+    public WebElement lastName;
+
+    @FindBy(xpath = "//input[@id='address']")
+    public WebElement address;
+
+    @FindBy(xpath = "//input[@id='mobilephone']")
+    public WebElement phoneNumber;
+
+    @FindBy(xpath = "//input[@id='username']")
+    public WebElement userName;
+
+    @FindBy(xpath = "//input[@id='email']")
+    public WebElement eMail;
+    @FindBy(xpath = "//input[@id='firstPassword']")
+    public WebElement password;
+    @FindBy(xpath = "//input[@id='secondPassword']")
+    public WebElement passwordConfirm;
+
+    @FindBy(xpath = "//button[@id='register-submit']//span[contains(text(),'Register')]")
+    public WebElement registerButton;
+
+    @FindBy(xpath = "//div[@id='app-view-container']//span[1]//span[1]")
+    public WebElement message;
+
+
+}
