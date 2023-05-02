1134e88b0fb27c3b1a51614614022d74c50adf77
MFethullahKoycu <mfethullahkoycu@gmail.com>
2023-05-02 02:16:57 +0200

    US14 unfinished

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -18,7 +18,7 @@ import org.junit.runner.RunWith;
 
         features = "src/test/resources",
         glue = "stepDefinitions",
-        tags = " @US01901",
+        tags = " @US01405",
         dryRun = false
 
 
---
+++ src/test/java/stepDefinitions/US024Stepdefs.java
@@ -0,0 +1,25 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import utilities.ConfigReader;
+
+public class US024Stepdefs {
+    @When("user creates state")
+    public void userCreatesState() {
+        Response response = RestAssured.given().auth().oauth2(ConfigReader.getProperty("token"))
+                .baseUri("https://www.gmibank.com/api/tp-states")
+                .and().pathParam("id","25370").get("/{id}")
+                .then().assertThat().body(Matchers.containsString("Caykara")).extract().response();
+
+
+        response.prettyPrint();
+    }
+
+    @Then("assert that state is created")
+    public void assertThatStateIsCreated() {
+    }
+}
